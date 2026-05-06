5ec017286e85fa7549551878fc254c1cba834403
azouzidigital-web <your-email@example.com>
2026-05-07 00:03:38 +0100

    Add Facebook Pixel ID 1592382432022194 for tracking

--- Primecast/src/components/client-side-scripts.tsx
+++ Primecast/src/components/client-side-scripts.tsx
@@ -47,6 +47,40 @@ export function ClientSideScripts() {
     return () => window.removeEventListener('load', registerSW);
   }, []);
   
+  // Facebook Pixel
+  useEffect(() => {
+    if (typeof window === 'undefined') return;
+    
+    const pixelScript = document.createElement('script');
+    pixelScript.innerHTML = `
+      !function(f,b,e,v,n,t,s)
+      {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+      if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+      n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t,s)}(window, document,'script',
+      'https://connect.facebook.net/en_US/fbevents.js');
+      fbq('init', '1592382432022194');
+      fbq('track', 'PageView');
+    `;
+    document.head.appendChild(pixelScript);
+    
+    // Noscript fallback for users without JavaScript
+    const noscript = document.createElement('noscript');
+    noscript.innerHTML = `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=1592382432022194&ev=PageView&noscript=1" />`;
+    document.head.appendChild(noscript);
+    
+    return () => {
+      if (pixelScript.parentNode) {
+        pixelScript.parentNode.removeChild(pixelScript);
+      }
+      if (noscript.parentNode) {
+        noscript.parentNode.removeChild(noscript);
+      }
+    };
+  }, []);
+  
   // Tawk.to Chat (separate component is being used for this)
   
   return null;
