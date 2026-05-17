7912fba6b448952f677ce44a2814ccb086f078b7
azouzidigital-web <your-email@example.com>
2026-05-17 20:26:57 +0100

    Add Google Analytics and Microsoft Clarity tracking scripts to site head

--- Primecast/src/app/layout.tsx
+++ Primecast/src/app/layout.tsx
@@ -58,6 +58,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-EW0TRWNHDZ"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-EW0TRWNHDZ');`
+          }}
+        />
         {/* Structured data json for organization */}
         <script
           type="application/ld+json"
@@ -77,6 +87,17 @@ export default function RootLayout({
                 "url": "https://veltrixbooks.com/contact"
               }
             })
+          }}
+        />
+        {/* Microsoft Clarity Tracking */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "wsnyz84s6s");`
           }}
         />
       </head>
